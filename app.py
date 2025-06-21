2ab5321c02e2c5e9ff0055db4bb4504e4b428a02
Vaidehi Pawar <vaidehipawar@Vaidehis-MacBook-Air.local>
2025-06-20 20:55:03 -0700

    Added assets demo video

--- app.py
+++ app.py
@@ -52,4 +52,4 @@ def predict():
         return render_template("index.html", columns=columns, prediction=f"Error: {str(e)}")
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=True, port=8001)
